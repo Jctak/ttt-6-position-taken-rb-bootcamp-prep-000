ab0698c9567360e76ca12cf435609116827d145b
Jeong Jin Tak <jeongjintak7@gmail.com>
2019-02-17 18:57:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
-# code your #position_taken? method here!
+books1 = "a"
+books2 ="b"
+books3 ="c"
+books = [books1, books2, books3]
+
+def print_titles(books)
+  books.each.times do |step|
+    puts books[step]
+  end
+end
+
+ 
+
